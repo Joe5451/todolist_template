2f5a68a6383b39bcf58b69a21eab512e68c42617
Joe <skybluejoe123@gmail.com>
2022-02-19 13:02:10 +0800

    init commit

---
+++ components/head.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="utf-8" lang="zh-TW" />
+    <meta http-equiv="Content-Language" content="zh-tw" />
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="expires" content="0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="distribution" content="Taiwan">
+    <meta name="robots" content="none">
+
+	<title>Todolist Template</title>
+
+	<base href="<?php //echo base_url(); ?>">
+
+    <link rel="stylesheet" href="css/icons.css" />
+    <link rel="stylesheet" href="css/style.css?<?php echo time(); ?>" />
+
+    <script language="javascript" src="js/jquery-1.12.3.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body>
+
+<header class="main_header px-5 py-3">
+    <nav class="main_nav flex items-center justify-end">
+        <a href="" class="main_nav_link">首頁</a>
+        <a href="" class="main_nav_link">分類管理</a>
+        <a href="" class="main_nav_link">垃圾桶</a>
+        <a href="" class="main_nav_link">登出</a>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,79 @@
+<?php include_once("components/head.php");  ?>
+
+<div class="container max-w-screen-lg px-4 py-8 mx-auto">
+    <div class="index_container rounded-lg">
+
+        <div class="todo_container">
+            <div class="todo_head">
+                <div class="todo_head_title">全部</div>
+                <div class="todo_header_btn_container">
+                    <button class="todo_head_btn active">待完成</button>
+                    <button class="todo_head_btn">完成</button>
+                    <button class="todo_head_btn">數據</button>
+                </div>
+            </div>
+            <div class="todo_body">
+                <div class="todolist_container">
+                    <div class="todolist_item">
+                        <div class="todolist_item_category_tag">Side Project</div>
+                        <div class="todolist_item_title">待辦 todolist side project 完成</div>
+                        <div class="todolist_item_content">完成 side project</div>
+                        <div class="todolist_item_btn_group">
+                            <button class="todolist_item_btn finish">
+                                <i class="fas fa-check-circle"></i>
+                            </button>
+                            <button class="todolist_item_btn delete">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="todolist_item">
+                        <div class="todolist_item_category_tag">Side Project</div>
+                        <div class="todolist_item_title">待辦 todolist side project 完成</div>
+                        <div class="todolist_item_content">完成 side project</div>
+                        <div class="todolist_item_btn_group">
+                            <button class="todolist_item_btn finish">
+                                <i class="fas fa-check-circle"></i>
+                            </button>
+                            <button class="todolist_item_btn delete">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="todolist_item">
+                        <div class="todolist_item_category_tag">Side Project</div>
+                        <div class="todolist_item_title">待辦 todolist side project 完成</div>
+                        <div class="todolist_item_content">完成 side project</div>
+                        <div class="todolist_item_btn_group">
+                            <button class="todolist_item_btn finish">
+                                <i class="fas fa-check-circle"></i>
+                            </button>
+                            <button class="todolist_item_btn delete">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="todolist_item">
+                        <div class="todolist_item_category_tag">Side Project</div>
+                        <div class="todolist_item_title">待辦 todolist side project 完成</div>
+                        <div class="todolist_item_content">完成 side project</div>
+                        <div class="todolist_item_btn_group">
+                            <button class="todolist_item_btn finish">
+                                <i class="fas fa-check-circle"></i>
+                            </button>
+                            <button class="todolist_item_btn delete">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+</div>
+
+<?php include_once("components/foot.php"); ?>
