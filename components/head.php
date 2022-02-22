fc2cba9c02f6392df4eba9d15d01162d406f59f5
Joe <skybluejoe123@gmail.com>
2022-02-22 23:30:23 +0800

    add mobile menu button

--- components/head.php
+++ components/head.php
@@ -27,7 +27,7 @@
 <body>
 
 <header class="main_header px-5 py-3">
-    <nav class="main_nav flex items-center justify-end">
+    <nav class="main_nav">
         <a href="index.php" class="main_nav_link">
             <i class="fas fa-list"></i>
             首頁
@@ -46,3 +46,37 @@
         </a>
     </nav>
 </header>
+
+<header id="mobile_menu" class="mobile_header px-5 py-3">
+    <nav class="mobile_nav">
+        <a href="index.php" class="mobile_nav_link">
+            <i class="fas fa-list"></i>
+            首頁
+        </a>
+        <a href="tag_list.php" class="mobile_nav_link">
+            <i class="fas fa-tags"></i>
+            標籤管理
+        </a>
+        <a href="trash.php" class="mobile_nav_link">
+            <i class="fas fa-trash-alt"></i>
+            垃圾桶
+        </a>
+        <a href="" class="mobile_nav_link">
+            <i class="fas fa-power-off"></i>
+            登出
+        </a>
+    </nav>
+</header>
+
+<div class="menu_btn">
+    <div class="menu_btn_bar menu_btn_bar1"></div>
+    <div class="menu_btn_bar menu_btn_bar2"></div>
+    <div class="menu_btn_bar menu_btn_bar3"></div>
+</div>
+
+<script>
+    $('.menu_btn').click(function() {
+        $(this).toggleClass('active');
+        $('#mobile_menu').stop().toggleClass('show');
+    });
+</script>
